6d454cd06c99f40591edaa747bfb00d08340baf2
Óscar García <oscarpoblacion@gmail.com>
2020-11-30 15:25:26 +0000

    Learn to speak JSON

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+from flask import jsonify
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-	return ("Hola Mundo, soy Python! Ahora con CloudBuild")
+	message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON"
+	response = {
+		"message": message,
+		"length": len(message)
+	}
+	return jsonify(response)
 
 @app.route('/bye')
 def bye_world():
